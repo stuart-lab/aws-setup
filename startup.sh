8c00764d074325fe50d25085ae85af0cdaca1e2f
timoast <4591688+timoast@users.noreply.github.com>
2023-02-23 15:27:51 +0800

    rename

---
+++ startup.sh
@@ -0,0 +1,24 @@
+# From Dave Tang:
+# https://davetang.org/muse/2022/12/07/running-rstudio-server-on-amazon-ec2/
+
+sudo apt-get update
+sudo apt-get install \
+    ca-certificates \
+    curl \
+    gnupg \
+    lsb-release \
+    unzip \
+    htop \
+    nvtop
+
+sudo mkdir -p /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+sudo apt-get update
+sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-compose-plugin
+
+sudo usermod -aG docker ubuntu
